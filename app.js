e413090e850ec949cc5d52b38dd00d4fe11a3663
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2025-03-18 15:20:23 +0200

    added the router folder and files and redid the routing method

--- app.js
+++ app.js
@@ -2,31 +2,15 @@ const express = require("express")
 const app = express()
 const path = require("node:path");
 
-const messages = [
-    {
-      text: "Hello There",
-      user: "Kenobi",
-      added: new Date()
-    },
-    {
-      text: "General Kenobi",
-      user: "Grevious",
-      added: new Date()
-    }
-  ];
-  
-
+const indexRouter = require("./routers/indexRouter")
+const newMessageRouter = require("./routers/newMessages")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/",(req,res) => {
-    res.render("index", {messages: messages})
-});
+app.use("/", indexRouter)
 
-app.get("/new", (req, res)=> {
-    res.render("newMessage")
-})
+app.use("/new", newMessageRouter);
 
 const PORT = 3000
 
---
+++ routers/indexRouter.js
@@ -0,0 +1,27 @@
+const {Router} = require("express")
+const indexRouter = Router();
+const express = require("express")
+const app = express()
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+  indexRouter.get("/", (req, res) => {
+    res.render("index", {title: "Mini MessageBoard", messages: messages})
+  })
+
+  module.exports = indexRouter
