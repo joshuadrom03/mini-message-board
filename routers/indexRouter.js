d40e731d09e5438024c9be8b9f417ab9df5c69de
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2025-03-18 14:33:40 +0200

    added the messages array to indexRouter

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,6 +2,21 @@ const {Router} = require("express")
 
 const indexRouter = Router();
 
+//create an array of sample messages
+const messages = [
+    {
+        text: "hello there",
+        user: "Kenobi",
+        added: new Date()
+    },
+    {
+        text: "General Kenobi!",
+        user: "Grevious",
+        added: new Date()
+    },
+
+]
+
 //create the index router routes
 indexRouter.get("/", (req, res)=> {
     res.send("Index Page")
